f19d309549edee390e4cb149d50977be8c1062f6
strahser <ssg-81@yandex.ru>
2023-08-14 22:19:07 +0300

    add data class

--- GSOPCalculator/GSOPProcess/CreateGSOPData.py
+++ GSOPCalculator/GSOPProcess/CreateGSOPData.py
@@ -5,17 +5,51 @@ from EnergyCompare.Utility.PandasFunctions import load_input_data_frame
 from GSOPCalculator.Static.GSOPNamesStatic import GSOPNamesStatic
 
 
+@dataclass		
+class CityProperty:
+	t_out:float
+	t_ot_per:float
+	z_ot_per:float
+
+@dataclass		
+class BuildingProperty:
+	t_in:float
+
+@dataclass		
+class GsopData:
+	gsop_value:float
+	gsop_str:str
+
 @dataclass
 class CalculateGSOP:
 	original_df: pd.DataFrame
 	selected_region: str = field(init=False)
-	selected_city: str = field(init=False)	
+	selected_city: str = field(init=False)
+	
+	
+	@property
+	def building_property(self,t_in=20):
+		_building_property = BuildingProperty(t_in)
+		return _building_property
+		
+	
+	@property
+	def city_property(self):
+		city = self._filter_df(self.df, GSOPNamesStatic.city, self.selected_city)
+		_city_property =CityProperty(
+		city[GSOPNamesStatic.t_out].values[0] ,
+		city[GSOPNamesStatic.t_ot_per].values[0],
+		city[GSOPNamesStatic.z_ot_per].values[0]
+		)
+		return _city_property
 	
 	@property
 	def gsop(self):
-		_city = self._filter_df(self.df, GSOPNamesStatic.city, self.selected_city)
-		_city = _city.assign(gsop=_city.apply(self.__calculate_gsop_function, axis=1))
-		return _city[GSOPNamesStatic.gsop].values[0]
+		_city = self.city_property
+		_gsop = (self.building_property.t_in - self.city_property.t_ot_per) * self.city_property.z_ot_per
+		_gsop_str = f"({self.building_property.t_in} - {self.city_property.t_ot_per}) * {self.city_property.z_ot_per} = {_gsop}"
+		_gsop_data =GsopData(_gsop,_gsop_str)
+		return _gsop_data
 	
 	@property
 	def unique_city(self) -> list:
@@ -41,6 +75,9 @@ class CalculateGSOP:
 		query = df[column_name] == column_value
 		return df[query]
 	
-	@staticmethod
-	def __calculate_gsop_function(df,t_in:int =20) -> float:
-		return (t_in - df[GSOPNamesStatic.t_ot_per]) * df[GSOPNamesStatic.z_ot_per]
+
+		
+
+	
+	
+	
--- GSOPCalculator/Static/GSOPNamesStatic.py
+++ GSOPCalculator/Static/GSOPNamesStatic.py
@@ -4,3 +4,4 @@ class GSOPNamesStatic:
     region:str ="Область"
     t_ot_per:str='tот.пер'
     z_ot_per:str='zот.пер.'
+    t_out:str = "tн"
